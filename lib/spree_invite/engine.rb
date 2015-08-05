abc1e6860a0f9fbee1815dc128c77e0fc9948069
Sean Carey <wycleffsean@gmail.com>
2015-08-05 01:40:46 -0400

    migrate thru engine

--- lib/spree_invite/engine.rb
+++ lib/spree_invite/engine.rb
@@ -9,6 +9,14 @@ module Spree
         end
       end
 
+      initializer :append_migrations do |app|
+        unless app.root.to_s.match root.to_s
+          config.paths["db/migrate"].expanded.each do |expanded_path|
+            app.config.paths["db/migrate"] << expanded_path
+          end
+        end
+      end
+
       config.to_prepare &method(:activate).to_proc
     end
   end
